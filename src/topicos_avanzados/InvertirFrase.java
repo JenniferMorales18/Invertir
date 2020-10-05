bb009c89f1b26c703a512146cb8a32577b9156f7
Jennifer Morales <jamm.jafm18@gmail.com>
2020-10-05 10:08:28 -0500

    Programa en Proceso de mejorar

---
+++ src/topicos_avanzados/InvertirFrase.java
@@ -0,0 +1,17 @@
+
+package topicos_avanzados;
+
+import java.util.Scanner;
+
+class InvertirFrase {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+        String frase;
+        System.out.println("Introduzca su frase:");
+        frase = sca.nextLine();
+        char[]conver = frase.toCharArray();
+        for(int x = conver.length-1; x !=-1; x--){
+            System.out.print(conver[x]);
+        }
+    }
+}
